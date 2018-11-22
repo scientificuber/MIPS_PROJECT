ce4b6bf9359be550537751e5965525d6bf3df37a
done-n-dusted <31320.anurag@gmail.com>
2018-11-22 19:44:28 +0530

    Updated input and output ports

--- mips-decoder-regs.v
+++ mips-decoder-regs.v
@@ -1,4 +1,4 @@
-module decoderAndRegisters(instruction, operand1, operand2);
+module decoderAndRegisters(instruction, s0, s1, s2, s3, s4, s5, s6, s7, t0, t1, t2, t3, t4, t5, t6, t7, MemR, MemW, RegW, MemToReg, aluControlLines, operand1, operand2);
     /*
     input [31:0] instruction;
     output [31:0] finalOut;
@@ -51,14 +51,25 @@ module decoderAndRegisters(instruction, operand1, operand2);
     aluMain ALU(out4, operand1, operand2, finalOut, zeroFlag);
     */
     input [31:0] instruction;
+    input [31:0] s0, s1, s2, s3, s4, s5, s6, s7;
+    input [31:0] t0, t1, t2, t3, t4, t5, t6, t7;
+
+    wire [31:0] S[7:0];
+    wire [31:0] T[7:0];
+
     output reg [31:0] operand1;
     output reg [31:0] operand2;
-    reg [31:0] S[7:0];
-    reg [31:0] T[7:0];
+    output MemR, MemW, RegW, MemToReg;
+    output [3:0] aluControlLines;
+
+    assign S[0] = s0; assign S[1] = s1; assign S[2] = s2; assign S[3] = s3; assign S[4] = s4; assign S[5] = s5; assign S[6] = s6; assign S[7] = s7;
+    assign T[0] = t0; assign T[1] = t1; assign T[2] = t2; assign T[3] = t3; assign T[4] = t4; assign T[5] = t5; assign T[6] = t6; assign T[7] = t7;
+
     /*
     reg [31:0] s0, s1, s2, s3, s4, s5, s6, s7;
     reg [31:0] t0, t1, t2, t3, t4, t5, t6, t7;
     */
+
     wire [5:0] opcode;
     assign opcode = instruction[31:26];
 
@@ -72,9 +83,6 @@ module decoderAndRegisters(instruction, operand1, operand2);
     assign foo = instruction[5:0];
 
     wire [1:0] aluop;
-
-    wire MemR, MemW, RegW, MemToReg;
-    wire [3:0] aluControlLines;
 
     controlUnit cu(opcode, aluop, MemR, MemW, RegW, MemToReg);
     aluControlUnit acu(aluop, foo, aluControlLines);
